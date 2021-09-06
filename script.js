ba730b1c3d931a00e46466198a3ff6e17e695150
Lucas <lcszraaad@gmail.com>
2021-09-06 15:38:20 -0300

    lcszraaad/calculadora

--- script.js
+++ script.js
@@ -22,14 +22,14 @@ const transactions=[
     description: 'Luz',
     amount: -50000,
     Date: '23/01/2021',
-    }
+    },
 
     {
     id: 2,
     description: 'Website',
     amount: -500000,
     Date: '23/01/2021',
-    }
+    },
 
     {
     id: 3,
@@ -48,7 +48,7 @@ const Transaction = {
 
     expenses(){
         //somar saidas
-    }
+    },
 
     total(){
         //Entradas menos saidas
@@ -57,15 +57,46 @@ const Transaction = {
 }
 
 const DOM = {
-    Element
-    innerHTMLTransaction() {
 
-        const html = "" 
-                    <tr>
-                        <td class="description">Luz</td>
-                        <td class="expense">- R$ 500,00</td>
-                        <td class="date">23/01/2021</td>
-                        <td><img src="./images/minus.svg" alt="Remover transação"></td>
-                    </tr>
+    transactionsContainer: document.querySelector('#data-tablet tbody'),
+    
+    addTransaction(transaction,index){
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+
+        
+
+    },
+    innerHTMLTransaction(transaction) {
+
+        const CSSclass = transaction.amount > 0  ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)  
+
+        const html = `
+        
+                
+                <td class="description">${transaction.description}</td>
+                <td class="${CSSclass}">${transaction.amount}</td>
+                <td class="date">${transaction.date}</td>
+                <td><img src="./images/minus.svg" alt="Remover transação"></td>
+                
+
+                     `
+
+                     return html
+
     }
 }
+
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : ""
+    }
+}
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
